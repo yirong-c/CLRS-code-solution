bbbdea9b03c66f9a19b4b64a9ed212ff006235a3
frc123 <i@frc6.com>
2021-03-18 19:47:53 +0800

    update to 10.3 alogrithm implementation 2

--- clrs/pointer_object.h
+++ clrs/pointer_object.h
@@ -6,9 +6,11 @@ class Allocater
 	{
 		FreeElement* next;
 	} FreeElement;
+
 	void* memory_addr_;
 	int single_element_size_;
 	FreeElement* free_list_head_;
+
 public:
 	Allocater(int single_element_size, int element_num);
 	~Allocater();
