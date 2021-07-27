43d3ff765c267b82c7ffb74836596b71dc63c57f
frc123 <i@frc6.com>
2021-07-27 03:44:17 -0700

    11.1-1

--- src/direct_address_tables.cpp
+++ src/direct_address_tables.cpp
@@ -6,25 +6,42 @@ struct ElementInSet
     void* satellite_data;
 };
 
-typedef ElementInSet** DirectAddressTable;
-
-ElementInSet* DirectAddressSearch(DirectAddressTable table, int key)
+ElementInSet* DirectAddressSearch(vector<ElementInSet*>& table, int key)
 {
     return table[key];
 }
 
-void DirectAddressInsert(DirectAddressTable table, ElementInSet* element)
+void DirectAddressInsert(vector<ElementInSet*>& table, ElementInSet* element)
 {
     table[element->key] = element;
 }
 
-void DirectAddressDelete(DirectAddressTable table, ElementInSet* element)
+void DirectAddressDelete(vector<ElementInSet*>& table, ElementInSet* element)
 {
     table[element->key] = NULL;
 }
 
+/*
+11.1-1
+O(n)
+*/
+ElementInSet* FindMaxElement(vector<ElementInSet*> table)
+{
+    int i;
+    ElementInSet* max;
+    for (i = 0; i < table.size(); ++i)
+    {
+        if (table[i] != NULL)
+            max = table[i];
+    }
+    return max;
+}
+
 int main()
 {
+    ElementInSet e0;
+    e0.key = 0;
+    e0.satellite_data = (void*)1111;
     ElementInSet e1;
     e1.key = 1;
     e1.satellite_data = (void*)1;
@@ -34,31 +51,34 @@ int main()
     ElementInSet e3;
     e3.key = 3;
     e3.satellite_data = (void*)3;
-    ElementInSet e4;
-    e4.key = 4;
-    e4.satellite_data = (void*)4;
-
-    ElementInSet* table[5];
-    DirectAddressInsert(table, &e3);
-    DirectAddressInsert(table, &e2);
-    DirectAddressInsert(table, &e1);
-    DirectAddressInsert(table, &e4);
 
     ElementInSet* result;
+
+    vector<ElementInSet*> table;
+    table.resize(4);
+    DirectAddressInsert(table, &e1);
+    result = FindMaxElement(table);
+    DirectAddressInsert(table, &e0);
+    result = FindMaxElement(table);
+    DirectAddressInsert(table, &e3);
+    result = FindMaxElement(table);
+    DirectAddressInsert(table, &e2);
+    result = FindMaxElement(table);
+
+    result = DirectAddressSearch(table, 0);
     result = DirectAddressSearch(table, 1);
     result = DirectAddressSearch(table, 2);
     result = DirectAddressSearch(table, 3);
-    result = DirectAddressSearch(table, 4);
 
+    DirectAddressDelete(table, &e0);
     DirectAddressDelete(table, &e1);
     DirectAddressDelete(table, &e2);
     DirectAddressDelete(table, &e3);
-    DirectAddressDelete(table, &e4);
 
+    result = DirectAddressSearch(table, 0);
     result = DirectAddressSearch(table, 1);
     result = DirectAddressSearch(table, 2);
     result = DirectAddressSearch(table, 3);
-    result = DirectAddressSearch(table, 4);
 
     return 0;
 }
