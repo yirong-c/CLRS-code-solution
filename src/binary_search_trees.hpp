ee326729a933070a1d36676c0068df59fca8bd76
frc123 <i@frc6.com>
2021-09-04 20:05:14 -0700

    12.3 (BST re-construct to oop)

--- src/binary_search_trees.hpp
+++ src/binary_search_trees.hpp
@@ -25,267 +25,307 @@ struct BinaryTreeNode
     }
 };
 
-//O(n)
 template <typename T1, typename T2>
-void InorderTreeWalk(BinaryTreeNode<T1, T2>* x)
+class BinarySearchTree
 {
-    if (x != nullptr)
-    {
-        InorderTreeWalk(x->left);
-        std::cout << x->key << std::endl;
-        InorderTreeWalk(x->right);
-    }
-}
+public:
+    BinaryTreeNode<T1, T2>* root_node_;
 
-//12.1-3
-//O(n)
-//space complexity: O(n)
-template <typename T1, typename T2>
-void InorderTreeWalkNonRecursive(BinaryTreeNode<T1, T2>* x)
-{
-    std::stack<BinaryTreeNode<T1, T2>*> stack;
-    while (true)
+    //O(n)
+    void InorderTreeWalkRecursive(BinaryTreeNode<T1, T2>* x)
     {
-        while (x != nullptr)
+        if (x != nullptr)
         {
-            stack.push(x);
-            x = x->left;
-        }
-        if (stack.empty())
-        {
-            break;
-        }
-        x = stack.top();
-        std::cout << x->key << std::endl;
-        stack.pop();
-        x = x->right;
-    }
-}
-
-//12.1-3
-//O(n)
-//space complexity: O(1)
-template <typename T1, typename T2>
-void InorderTreeWalkNonRecursiveNonStack(BinaryTreeNode<T1, T2>* x)
-{
-    BinaryTreeNode<T1, T2>* last;
-    goto Start;
-    while (x->parent)
-    {
-        last = x;
-        x = x->parent;
-        if (last == x->left)
-        {
+            InorderTreeWalkRecursive(x->left);
             std::cout << x->key << std::endl;
-            while (x->right)
+            InorderTreeWalkRecursive(x->right);
+        }
+    }
+
+    //O(n)
+    void InorderTreeWalkRecursive()
+    {
+        InorderTreeWalkRecursive(root_node_);
+    }
+
+    //12.1-3
+    //O(n)
+    //space complexity: O(n)
+    void InorderTreeWalkNonRecursive()
+    {
+        std::stack<BinaryTreeNode<T1, T2>*> stack;
+        BinaryTreeNode<T1, T2>* x;
+        x = root_node_;
+        while (true)
+        {
+            while (x != nullptr)
             {
-                x = x->right;
-                Start:
-                while (x->left)
-                {
-                    x = x->left;
-                }
+                stack.push(x);
+                x = x->left;
+            }
+            if (stack.empty())
+            {
+                break;
+            }
+            x = stack.top();
+            std::cout << x->key << std::endl;
+            stack.pop();
+            x = x->right;
+        }
+    }
+
+    //12.1-3
+    //O(n)
+    //space complexity: O(1)
+    void InorderTreeWalkNonRecursiveNonStack()
+    {
+        BinaryTreeNode<T1, T2> *last, *x;
+        x = root_node_;
+        goto Start;
+        while (x->parent)
+        {
+            last = x;
+            x = x->parent;
+            if (last == x->left)
+            {
                 std::cout << x->key << std::endl;
+                while (x->right)
+                {
+                    x = x->right;
+                    Start:
+                    while (x->left)
+                    {
+                        x = x->left;
+                    }
+                    std::cout << x->key << std::endl;
+                }
             }
         }
     }
-}
 
-//12.1-4
-//O(n)
-template <typename T1, typename T2>
-void PreorderTreeWalk(BinaryTreeNode<T1, T2>* x)
-{
-    if (x != nullptr)
+    //12.1-4
+    //O(n)
+    void PreorderTreeWalkRecursive(BinaryTreeNode<T1, T2>* x)
     {
-        std::cout << x->key << std::endl;
-        PreorderTreeWalk(x->left);
-        PreorderTreeWalk(x->right);
-    }
-}
-
-//12.1-4
-//O(n)
-template <typename T1, typename T2>
-void PostorderTreeWalk(BinaryTreeNode<T1, T2>* x)
-{
-    if (x != nullptr)
-    {
-        PostorderTreeWalk(x->left);
-        PostorderTreeWalk(x->right);
-        std::cout << x->key << std::endl;
-    }
-}
-
-//O(h)
-//h: height (h >= lgn)
-//space complexity: O(h)
-template <typename T1, typename T2>
-BinaryTreeNode<T1, T2>* TreeSearch(BinaryTreeNode<T1, T2>* root_node, T2 key)
-{
-    if (root_node == nullptr || root_node->key == key)
-        return root_node;
-    if (key < root_node->key)
-        return TreeSearch(root_node->left, key);
-    else
-        return TreeSearch(root_node->right, key);
-}
-
-//O(h)
-//h: height (h >= lgn)
-//space complexity: O(1)
-template <typename T1, typename T2>
-BinaryTreeNode<T1, T2>* IterativeTreeSearch(BinaryTreeNode<T1, T2>* root_node, T2 key)
-{
-    while (root_node != nullptr && root_node->key != key)
-    {
-        if (key < root_node->key)
-            root_node = root_node->left;
-        else
-            root_node = root_node->right;
-    }    
-    return root_node;
-}
-
-//O(h)
-//root_node cannot be nullptr
-template <typename T1, typename T2>
-BinaryTreeNode<T1, T2>* TreeMinimum(BinaryTreeNode<T1, T2>* root_node)
-{
-    while (root_node->left)
-        root_node = root_node->left;
-    return root_node;
-}
-
-//O(h)
-//root_node cannot be nullptr
-template <typename T1, typename T2>
-BinaryTreeNode<T1, T2>* TreeMaximum(BinaryTreeNode<T1, T2>* root_node)
-{
-    while (root_node->right)
-        root_node = root_node->right;
-    return root_node;
-}
-
-//O(h)
-//node cannot be nullptr
-template <typename T1, typename T2>
-BinaryTreeNode<T1, T2>* TreeSuccessor(BinaryTreeNode<T1, T2>* node)
-{
-    BinaryTreeNode<T1, T2>* parent;
-    if (node->right != nullptr)
-        return TreeMinimum(node->right);
-    parent = node->parent;
-    while (parent != nullptr && parent->right == node)
-    {
-        node = parent;
-        parent = node->parent;
-    }
-    return parent;
-}
-
-//12.2-2
-//O(h)
-//root_node cannot be nullptr
-template <typename T1, typename T2>
-BinaryTreeNode<T1, T2>* TreeMinimumRecursive(BinaryTreeNode<T1, T2>* root_node)
-{
-    if (root_node->left)
-        return TreeMinimumRecursive(root_node->left);
-    else
-        return root_node;
-}
-
-//12.2-2
-//O(h)
-//root_node cannot be nullptr
-template <typename T1, typename T2>
-BinaryTreeNode<T1, T2>* TreeMaximumRecursive(BinaryTreeNode<T1, T2>* root_node)
-{
-    if (root_node->right)
-        return TreeMaximumRecursive(root_node->right);
-    else
-        return root_node;
-}
-
-//12.2-3
-//O(h)
-//node cannot be nullptr
-template <typename T1, typename T2>
-BinaryTreeNode<T1, T2>* TreePredecessor(BinaryTreeNode<T1, T2>* node)
-{
-    BinaryTreeNode<T1, T2>* parent;
-    if (node->left)
-        return TreeMaximum(node->left);
-    parent = node->parent;
-    while (parent && parent->left == node)
-    {
-        node = parent;
-        parent = node->parent;
-    }
-    return parent;
-}
-
-//O(h)
-//root_node cannot be nullptr
-template<typename T1, typename T2>
-void TreeInsert(BinaryTreeNode<T1, T2>* root_node, BinaryTreeNode<T1, T2>* to_insert)
-{
-    //treat root_node as "now_node"
-    while (root_node != nullptr)
-    {
-        to_insert->parent = root_node;
-        if (to_insert->key < root_node->key)
-            root_node = root_node->left;
-        else
-            root_node = root_node->right;
-    }
-    if (to_insert->key < to_insert->parent->key)
-        to_insert->parent->left = to_insert;
-    else
-        to_insert->parent->right = to_insert;
-}
-
-//replace subtree rooted at node u with subtree rooted at node v
-//u and u->parent cannot be nullptr
-template <typename T1, typename T2>
-void Transplant(BinaryTreeNode<T1, T2>* u, BinaryTreeNode<T1, T2>* v)
-{
-    if (u == u->parent->left)
-        u->parent->left = v;
-    else
-        u->parent->right = v;
-    if (v != nullptr)
-        v->parent = u->parent;
-    //u->parent = nullptr;
-}
-
-//O(h)
-//to_delete cannot be nullptr
-template <typename T1, typename T2>
-void TreeDelete(BinaryTreeNode<T1, T2>* to_delete)
-{
-    BinaryTreeNode<T1, T2>* will_replace;
-    if (to_delete->left == nullptr)
-    {
-        Transplant(to_delete, to_delete->right);
-    }
-    else if (to_delete->right == nullptr)
-    {
-        Transplant(to_delete, to_delete->left);
-    }
-    else
-    {
-        will_replace = TreeMinimum(to_delete->right);
-        if (will_replace->parent != to_delete)
+        if (x != nullptr)
         {
-            Transplant(will_replace, will_replace->right);
-            will_replace->right = to_delete->right;
-            will_replace->right->parent = will_replace;
+            std::cout << x->key << std::endl;
+            PreorderTreeWalkRecursive(x->left);
+            PreorderTreeWalkRecursive(x->right);
         }
-        Transplant(to_delete, will_replace);
-        will_replace->left = to_delete->left;
-        will_replace->left->parent = will_replace;
     }
-}
+
+    //12.1-4
+    //O(n)
+    void PreorderTreeWalkRecursive()
+    {
+        PreorderTreeWalkRecursive(root_node_);
+    }
+
+    //12.1-4
+    //O(n)
+    void PostorderTreeWalkRecursive(BinaryTreeNode<T1, T2>* x)
+    {
+        if (x != nullptr)
+        {
+            PostorderTreeWalkRecursive(x->left);
+            PostorderTreeWalkRecursive(x->right);
+            std::cout << x->key << std::endl;
+        }
+    }
+
+    //12.1-4
+    //O(n)
+    void PostorderTreeWalkRecursive()
+    {
+        PostorderTreeWalkRecursive(root_node_);
+    }
+
+    //O(h)
+    //h: height (h >= lgn)
+    //space complexity: O(h)
+    BinaryTreeNode<T1, T2>* TreeSearchRecursive(BinaryTreeNode<T1, T2>* root_node, T2 key)
+    {
+        if (root_node == nullptr || root_node->key == key)
+            return root_node;
+        if (key < root_node->key)
+            return TreeSearchRecursive(root_node->left, key);
+        else
+            return TreeSearchRecursive(root_node->right, key);
+    }
+
+    //O(h)
+    //h: height (h >= lgn)
+    //space complexity: O(h)
+    BinaryTreeNode<T1, T2>* TreeSearchRecursive(T2 key)
+    {
+        return TreeSearchRecursive(root_node_, key);
+    }
+
+    //O(h)
+    //h: height (h >= lgn)
+    //space complexity: O(1)
+    BinaryTreeNode<T1, T2>* IterativeTreeSearch(BinaryTreeNode<T1, T2>* root_node, T2 key)
+    {
+        while (root_node != nullptr && root_node->key != key)
+        {
+            if (key < root_node->key)
+                root_node = root_node->left;
+            else
+                root_node = root_node->right;
+        }    
+        return root_node;
+    }
+
+    //O(h)
+    //h: height (h >= lgn)
+    //space complexity: O(1)
+    BinaryTreeNode<T1, T2>* IterativeTreeSearch(T2 key)
+    {
+        return IterativeTreeSearch(root_node_, key);
+    }
+
+    //O(h)
+    //root_node cannot be nullptr
+    BinaryTreeNode<T1, T2>* TreeMinimum(BinaryTreeNode<T1, T2>* root_node)
+    {
+        while (root_node->left)
+            root_node = root_node->left;
+        return root_node;
+    }
+
+    //O(h)
+    //root_node cannot be nullptr
+    BinaryTreeNode<T1, T2>* TreeMaximum(BinaryTreeNode<T1, T2>* root_node)
+    {
+        while (root_node->right)
+            root_node = root_node->right;
+        return root_node;
+    }
+
+    //O(h)
+    //node cannot be nullptr
+    BinaryTreeNode<T1, T2>* TreeSuccessor(BinaryTreeNode<T1, T2>* node)
+    {
+        BinaryTreeNode<T1, T2>* parent;
+        if (node->right != nullptr)
+            return TreeMinimum(node->right);
+        parent = node->parent;
+        while (parent != nullptr && parent->right == node)
+        {
+            node = parent;
+            parent = node->parent;
+        }
+        return parent;
+    }
+
+    //12.2-2
+    //O(h)
+    //root_node cannot be nullptr
+    BinaryTreeNode<T1, T2>* TreeMinimumRecursive(BinaryTreeNode<T1, T2>* root_node)
+    {
+        if (root_node->left)
+            return TreeMinimumRecursive(root_node->left);
+        else
+            return root_node;
+    }
+
+    //12.2-2
+    //O(h)
+    //root_node cannot be nullptr
+    BinaryTreeNode<T1, T2>* TreeMaximumRecursive(BinaryTreeNode<T1, T2>* root_node)
+    {
+        if (root_node->right)
+            return TreeMaximumRecursive(root_node->right);
+        else
+            return root_node;
+    }
+
+    //12.2-3
+    //O(h)
+    //node cannot be nullptr
+    BinaryTreeNode<T1, T2>* TreePredecessor(BinaryTreeNode<T1, T2>* node)
+    {
+        BinaryTreeNode<T1, T2>* parent;
+        if (node->left)
+            return TreeMaximum(node->left);
+        parent = node->parent;
+        while (parent && parent->left == node)
+        {
+            node = parent;
+            parent = node->parent;
+        }
+        return parent;
+    }
+
+    //O(h)
+    void TreeInsert(BinaryTreeNode<T1, T2>* to_insert)
+    {
+        BinaryTreeNode<T1, T2>* now;
+        if (root_node_ == nullptr)
+        {
+            root_node_ = to_insert;
+        }
+        else
+        {
+            now = root_node_;
+            while (now)
+            {
+                to_insert->parent = now;
+                if (to_insert->key < now->key)
+                    now = now->left;
+                else
+                    now = now->right;
+            }
+            if (to_insert->key < to_insert->parent->key)
+                to_insert->parent->left = to_insert;
+            else
+                to_insert->parent->right = to_insert;
+        }
+    }
+
+    //replace subtree rooted at node u with subtree rooted at node v
+    //u and u->parent cannot be nullptr
+    void Transplant(BinaryTreeNode<T1, T2>* u, BinaryTreeNode<T1, T2>* v)
+    {
+        if (u->parent == nullptr)
+            root_node_ = v;
+        else if (u == u->parent->left)
+            u->parent->left = v;
+        else
+            u->parent->right = v;
+        if (v != nullptr)
+            v->parent = u->parent;
+        //u->parent = nullptr;
+    }
+
+    //O(h)
+    //to_delete cannot be nullptr
+    void TreeDelete(BinaryTreeNode<T1, T2>* to_delete)
+    {
+        BinaryTreeNode<T1, T2>* will_replace;
+        if (to_delete->left == nullptr)
+        {
+            Transplant(to_delete, to_delete->right);
+        }
+        else if (to_delete->right == nullptr)
+        {
+            Transplant(to_delete, to_delete->left);
+        }
+        else
+        {
+            will_replace = TreeMinimum(to_delete->right);
+            if (will_replace->parent != to_delete)
+            {
+                Transplant(will_replace, will_replace->right);
+                will_replace->right = to_delete->right;
+                will_replace->right->parent = will_replace;
+            }
+            Transplant(to_delete, will_replace);
+            will_replace->left = to_delete->left;
+            will_replace->left->parent = will_replace;
+        }
+    }
+
+};
 
--- src/binary_search_trees_test.cpp
+++ src/binary_search_trees_test.cpp
@@ -5,6 +5,7 @@ int main()
     //build test tree
     typedef BinaryTreeNode<int, int> TestNode;
     typedef TestNode* TestNodePtr;
+    BinarySearchTree<int, int> bst;
     TestNode n1(1), n2(2), n3(3), n4(4), n5(5), n6(6), n7(7), 
         n8(8), n9(9), n10(10), n11(11), n12(12), n13(13), 
         n14(14), n15(15), n16(16);
@@ -25,43 +26,46 @@ int main()
     n15.SetRelation(&n12, &n13, &n16);
     n16.SetRelation(&n15, nullptr, nullptr);
 
+    bst.root_node_ = &n10;
+
     //test functions
-    InorderTreeWalkNonRecursive(&n10);
+    bst.InorderTreeWalkRecursive();
     std::cout << "---" << std::endl;
-    InorderTreeWalkNonRecursiveNonStack(&n10);
-
+    bst.InorderTreeWalkNonRecursive();
+    std::cout << "---" << std::endl;
+    bst.InorderTreeWalkNonRecursiveNonStack();
+    std::cout << "---" << std::endl;
+    
+    bst.PreorderTreeWalkRecursive();
+    std::cout << "---" << std::endl;
+    bst.PostorderTreeWalkRecursive(&n10);
     std::cout << "---" << std::endl;
 
-    PreorderTreeWalk(&n10);
-    std::cout << "---" << std::endl;
-    PostorderTreeWalk(&n10);
+    std::cout << "TreeMinimum(&n10)->key: " << bst.TreeMinimum(&n10)->key << std::endl;
+    std::cout << "TreeMaximum(&n10)->key: " << bst.TreeMaximum(&n10)->key << std::endl;
 
-    std::cout << "---" << std::endl;
+    std::cout << "TreeSuccessor(&n4)->key: " << bst.TreeSuccessor(&n4)->key << std::endl;
+    std::cout << "TreeSuccessor(&n10)->key: " << bst.TreeSuccessor(&n10)->key << std::endl;
+    std::cout << "TreeSuccessor(&n11)->key: " << bst.TreeSuccessor(&n11)->key << std::endl;
+    std::cout << "TreeSuccessor(&n16) == nullptr: " << (bst.TreeSuccessor(&n16) == nullptr) << std::endl;
 
-    std::cout << "TreeMinimum(&n10)->key: " << TreeMinimum(&n10)->key << std::endl;
-    std::cout << "TreeMaximum(&n10)->key: " << TreeMaximum(&n10)->key << std::endl;
+    std::cout << "TreeMinimumRecursive(&n10)->key: " << bst.TreeMinimumRecursive(&n10)->key << std::endl;
+    std::cout << "TreeMaximumRecursive(&n10)->key: " << bst.TreeMaximumRecursive(&n10)->key << std::endl;
 
-    std::cout << "TreeSuccessor(&n4)->key: " << TreeSuccessor(&n4)->key << std::endl;
-    std::cout << "TreeSuccessor(&n10)->key: " << TreeSuccessor(&n10)->key << std::endl;
-    std::cout << "TreeSuccessor(&n11)->key: " << TreeSuccessor(&n11)->key << std::endl;
-    std::cout << "TreeSuccessor(&n16) == nullptr: " << (TreeSuccessor(&n16) == nullptr) << std::endl;
-
-    std::cout << "TreeMinimumRecursive(&n10)->key: " << TreeMinimumRecursive(&n10)->key << std::endl;
-    std::cout << "TreeMaximumRecursive(&n10)->key: " << TreeMaximumRecursive(&n10)->key << std::endl;
-
-    std::cout << "TreePredecessor(&n1) == nullptr: " << (TreePredecessor(&n1) == nullptr) << std::endl;
-    std::cout << "TreePredecessor(&n2)->key: " << TreePredecessor(&n2)->key << std::endl;
-    std::cout << "TreePredecessor(&n4)->key: " << TreePredecessor(&n4)->key << std::endl;
+    std::cout << "TreePredecessor(&n1) == nullptr: " << (bst.TreePredecessor(&n1) == nullptr) << std::endl;
+    std::cout << "TreePredecessor(&n2)->key: " << bst.TreePredecessor(&n2)->key << std::endl;
+    std::cout << "TreePredecessor(&n4)->key: " << bst.TreePredecessor(&n4)->key << std::endl;
     //std::cout << "TreePredecessor(&n7)->key: " << TreePredecessor(&n7)->key << std::endl;
-    std::cout << "TreePredecessor(&n10)->key: " << TreePredecessor(&n10)->key << std::endl;
-    std::cout << "TreePredecessor(&n11)->key: " << TreePredecessor(&n11)->key << std::endl;
-    std::cout << "TreePredecessor(&n16)->key: " << TreePredecessor(&n16)->key << std::endl;
+    std::cout << "TreePredecessor(&n10)->key: " << bst.TreePredecessor(&n10)->key << std::endl;
+    std::cout << "TreePredecessor(&n11)->key: " << bst.TreePredecessor(&n11)->key << std::endl;
+    std::cout << "TreePredecessor(&n16)->key: " << bst.TreePredecessor(&n16)->key << std::endl;
 
-    TreeInsert(&n10, &n7);
+    bst.TreeInsert(&n7);
 
-    TreeDelete(&n12);
-    TreeDelete(&n3);
-    //TreeDelete(&n10);
-    InorderTreeWalkNonRecursiveNonStack(&n10);
+    bst.TreeDelete(&n12);
+    bst.TreeDelete(&n3);
+    bst.TreeDelete(&n10);
+    bst.InorderTreeWalkNonRecursiveNonStack();
+
     return 0;
 }
